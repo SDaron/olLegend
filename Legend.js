e558bd299f5849f1dc23bad4f938f22d297c2919
Simon Daron <simon@surlaterre.org>
2020-05-09 08:51:07 +0200

    first commit

---
+++ Legend.js
@@ -0,0 +1,361 @@
+/**
+ * @module ol/control/Legend
+ */
+import {equals} from 'ol/array.js';
+import Control from 'ol/control/Control.js';
+import {CLASS_CONTROL, CLASS_UNSELECTABLE, CLASS_COLLAPSED} from 'ol/css.js';
+import {removeChildren, replaceNode} from 'ol/dom.js';
+import EventType from 'ol/events/EventType.js';
+import {inView} from 'ol/layer/Layer.js';
+import {toContext} from 'ol/render';
+import {LineString, Point, Polygon} from 'ol/geom';
+import {getVectorContext} from 'ol/render.js';
+
+
+/**
+ * @typedef {Object} Options
+ * @property {string} [className='ol-legend'] CSS class name.
+ * @property {HTMLElement|string} [target] Specify a target if you
+ * want the control to be rendered outside of the map's
+ * viewport.
+ * @property {boolean} [collapsible] Specify if legends can
+ * be collapsed. If not specified, sources control this behavior with their
+ * `legendsCollapsible` setting.
+ * @property {boolean} [collapsed=true] Specify if legends should
+ * be collapsed at startup.
+ * @property {string} [tipLabel='Legends'] Text label to use for the button tip.
+ * @property {string} [label='i'] Text label to use for the
+ * collapsed legends button.
+ * Instead of text, also an element (e.g. a `span` element) can be used.
+ * @property {string|HTMLElement} [collapseLabel='»'] Text label to use
+ * for the expanded legends button.
+ * Instead of text, also an element (e.g. a `span` element) can be used.
+ * @property {function(import("../MapEvent.js").default)} [render] Function called when
+ * the control should be re-rendered. This is called in a `requestAnimationFrame`
+ * callback.
+ */
+
+
+/**
+ * @classdesc
+ * Control to show all the legends associated with the layer sources
+ * in the map. This control is one of the default controls included in maps.
+ * By default it will show in the bottom right portion of the map, but this can
+ * be changed by using a css selector for `.ol-legend`.
+ *
+ * @api
+ */
+class Legend extends Control {
+
+  /**
+   * @param {Options=} opt_options Legend options.
+   */
+  constructor(opt_options) {
+
+    const options = opt_options ? opt_options : {};
+
+    super({
+      element: document.createElement('div'),
+      render: options.render || render,
+      target: options.target
+    });
+
+    /**
+     * @private
+     * @type {HTMLElement}
+     */
+    this.ulElement_ = document.createElement('ul');
+
+    /**
+     * @private
+     * @type {boolean}
+     */
+    this.collapsed_ = options.collapsed !== undefined ? options.collapsed : true;
+
+    /**
+     * @private
+     * @type {boolean}
+     */
+    this.overrideCollapsible_ = options.collapsible !== undefined;
+
+    /**
+     * @private
+     * @type {boolean}
+     */
+    this.collapsible_ = options.collapsible !== undefined ?
+      options.collapsible : true;
+
+    if (!this.collapsible_) {
+      this.collapsed_ = false;
+    }
+
+    const className = options.className !== undefined ? options.className : 'ol-legend';
+
+    const tipLabel = options.tipLabel !== undefined ? options.tipLabel : 'Legends';
+
+    const collapseLabel = options.collapseLabel !== undefined ? options.collapseLabel : '\u00BB';
+
+    if (typeof collapseLabel === 'string') {
+      /**
+       * @private
+       * @type {HTMLElement}
+       */
+      this.collapseLabel_ = document.createElement('span');
+      this.collapseLabel_.textContent = collapseLabel;
+    } else {
+      this.collapseLabel_ = collapseLabel;
+    }
+
+    const label = options.label !== undefined ? options.label : 'i';
+
+    if (typeof label === 'string') {
+      /**
+       * @private
+       * @type {HTMLElement}
+       */
+      this.label_ = document.createElement('span');
+      this.label_.textContent = label;
+    } else {
+      this.label_ = label;
+    }
+
+
+    const activeLabel = (this.collapsible_ && !this.collapsed_) ?
+      this.collapseLabel_ : this.label_;
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.title = tipLabel;
+    button.appendChild(activeLabel);
+
+    button.addEventListener(EventType.CLICK, this.handleClick_.bind(this), false);
+
+    const cssClasses = className + ' ' + CLASS_UNSELECTABLE + ' ' + CLASS_CONTROL +
+        (this.collapsed_ && this.collapsible_ ? ' ' + CLASS_COLLAPSED : '') +
+        (this.collapsible_ ? '' : ' ol-uncollapsible');
+    const element = this.element;
+    element.className = cssClasses;
+    element.appendChild(this.ulElement_);
+    element.appendChild(button);
+
+    /**
+     * A list of currently rendered resolutions.
+     * @type {Array<string>}
+     * @private
+     */
+    this.renderedLegends_ = [];
+
+    /**
+     * @private
+     * @type {boolean}
+     */
+    this.renderedVisible_ = true;
+
+  }
+
+  /**
+   * Collect a list of visible legends and set the collapsible state.
+   * @param {import("../PluggableMap.js").FrameState} frameState Frame state.
+   * @return {Array<string>} Legends.
+   * @private
+   */
+  collectSourceLegends_(frameState) {
+    /**
+     * Used to determine if an legend already exists.
+     * @type {!Object<string, boolean>}
+     */
+    const lookup = {};
+
+    /**
+     * A list of visible legends.
+     * @type {Array<string>}
+     */
+    const visibleLegends = [];
+
+    const layerStatesArray = frameState.layerStatesArray;
+    for (let i = 0, ii = layerStatesArray.length; i < ii; ++i) {
+      const layerState = layerStatesArray[i];
+      if (!inView(layerState, frameState.viewState)) {
+        continue;
+      }
+
+      const source = /** @type {import("../layer/Layer.js").default} */ (layerState.layer).getSource();
+      if (!source) {
+        continue;
+      }
+
+      const legendGetter = source.getLegends();
+      if (!legendGetter) {
+        continue;
+      }
+
+      const legends = legendGetter(frameState);
+      if (!legends) {
+        continue;
+      }
+
+
+      if (Array.isArray(legends)) {
+        for (let j = 0, jj = legends.length; j < jj; ++j) {
+          visibleLegends.push(this.createLegendItem_(legends[j].label, legends[j].style, legends[j].geometry));
+          lookup[legends[j]] = true;
+        }
+      } else {
+        visibleLegends.push(this.createLegendItem_(legends.label,legends.style,legends.geometry));
+        lookup[legends] = true;
+      }
+    }
+    return visibleLegends;
+  }
+
+  /**
+   * Create a legend item.
+   * @param {string} label Label of the legend.
+   * @param {import("../../style/Style.js").default|Array<import("../../style/Style.js").default>} styles The style or array of styles.
+   * @return {Array<string>} Legends.
+   * @private
+   */
+  createLegendItem_(label,style,geometry) {
+    const legendItem = document.createElement('li');
+    const legendItemCanvas = document.createElement('canvas');
+    const legendItemLabel = document.createElement('span');
+    legendItemLabel.textContent = label;
+    
+
+    const vectorContext = toContext(legendItemCanvas.getContext('2d'), {size: [30, 30]});
+    vectorContext.setStyle(style);
+    
+    if(geometry == 'Polygon'){
+      vectorContext.drawGeometry(new Polygon([[[2, 6], [15, 6], [28, 2], [26, 24], [10, 20], [4, 26], [2, 2]]]));
+    }else if(geometry == 'LineString'){
+      vectorContext.drawGeometry((new LineString([[0, 15], [6, 10], [12, 20], [18, 18], [24,15], [30,15]])));
+    }else{
+      vectorContext.drawGeometry(new Point([15, 15]));
+    }
+    
+    
+    legendItem.appendChild(legendItemCanvas);
+    legendItem.appendChild(legendItemLabel);
+    
+    return legendItem;
+  }
+  
+  /**
+   * @private
+   * @param {?import("../PluggableMap.js").FrameState} frameState Frame state.
+   */
+  updateElement_(frameState) {
+    if (!frameState) {
+      if (this.renderedVisible_) {
+        this.element.style.display = 'none';
+        this.renderedVisible_ = false;
+      }
+      return;
+    }
+
+    const legends = this.collectSourceLegends_(frameState);
+
+    const visible = legends.length > 0;
+    if (this.renderedVisible_ != visible) {
+      this.element.style.display = visible ? '' : 'none';
+      this.renderedVisible_ = visible;
+    }
+
+    if (equals(legends, this.renderedLegends_)) {
+      return;
+    }
+
+    removeChildren(this.ulElement_);
+
+    // append the legends
+    for (let i = 0, ii = legends.length; i < ii; ++i) {
+      const element = legends[i];
+      this.ulElement_.appendChild(element);
+    }
+
+    this.renderedLegends_ = legends;
+  }
+
+  /**
+   * @param {MouseEvent} event The event to handle
+   * @private
+   */
+  handleClick_(event) {
+    event.preventDefault();
+    this.handleToggle_();
+  }
+
+  /**
+   * @private
+   */
+  handleToggle_() {
+    this.element.classList.toggle(CLASS_COLLAPSED);
+    if (this.collapsed_) {
+      replaceNode(this.collapseLabel_, this.label_);
+    } else {
+      replaceNode(this.label_, this.collapseLabel_);
+    }
+    this.collapsed_ = !this.collapsed_;
+  }
+
+  /**
+   * Return `true` if the legend is collapsible, `false` otherwise.
+   * @return {boolean} True if the widget is collapsible.
+   * @api
+   */
+  getCollapsible() {
+    return this.collapsible_;
+  }
+
+  /**
+   * Set whether the legend should be collapsible.
+   * @param {boolean} collapsible True if the widget is collapsible.
+   * @api
+   */
+  setCollapsible(collapsible) {
+    if (this.collapsible_ === collapsible) {
+      return;
+    }
+    this.collapsible_ = collapsible;
+    this.element.classList.toggle('ol-uncollapsible');
+    if (!collapsible && this.collapsed_) {
+      this.handleToggle_();
+    }
+  }
+
+  /**
+   * Collapse or expand the legend according to the passed parameter. Will
+   * not do anything if the legend isn't collapsible or if the current
+   * collapsed state is already the one requested.
+   * @param {boolean} collapsed True if the widget is collapsed.
+   * @api
+   */
+  setCollapsed(collapsed) {
+    if (!this.collapsible_ || this.collapsed_ === collapsed) {
+      return;
+    }
+    this.handleToggle_();
+  }
+
+  /**
+   * Return `true` when the legend is currently collapsed or `false`
+   * otherwise.
+   * @return {boolean} True if the widget is collapsed.
+   * @api
+   */
+  getCollapsed() {
+    return this.collapsed_;
+  }
+}
+
+
+/**
+ * Update the legend element.
+ * @param {import("../MapEvent.js").default} mapEvent Map event.
+ * @this {Legend}
+ */
+export function render(mapEvent) {
+  this.updateElement_(mapEvent.frameState);
+}
+
+
+export default Legend;
---
+++ index.js
@@ -0,0 +1,261 @@
+
+import 'ol/ol.css';
+import '@fortawesome/fontawesome-free/css/all.css'
+
+import GeoJSON from 'ol/format/GeoJSON.js';
+import {Map, View} from 'ol';
+import {Icon, RegularShape, Fill, Circle, Style, Stroke, Text} from 'ol/style';
+import {Raster as RasterSource, OSM, Stamen, Vector as VectorSource} from 'ol/source.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import {fromLonLat} from 'ol/proj.js';
+import {getVectorContext} from 'ol/render.js';
+import {LineString, Point, Polygon} from 'ol/geom';
+import {toContext} from 'ol/render';
+import controlLegend from './Legend.js';
+import './legend.css';
+import history from './maelbeek_history.json';
+import hydrology from './maelbeek_hydrology.json';
+
+
+//A distinct className is required to use another canvas for the background
+const background = new TileLayer({
+  opacity: 0.3,
+  className: 'stamen',
+  source: new Stamen({
+    layer: 'toner-lines',
+  }),
+});
+
+const base = new TileLayer({
+  opacity: 1,
+  source: new Stamen({
+    layer: 'toner-background',
+  }),
+});
+
+const clipLayer = new VectorLayer({
+  style: new Style({
+    stroke: new Stroke({
+      width:3,
+      color: 'red',
+    }),
+  }),
+  source: new VectorSource({
+    features: (new GeoJSON()).readFeatures(hydrology,{ featureProjection: 'EPSG:3857'}),
+    format: new GeoJSON(),
+  }),
+});
+
+//Giving the clipped layer an extent is necessary to avoid rendering when the feature is outside the viewport
+clipLayer.getSource().on('addfeature', function () {
+  base.setExtent(clipLayer.getSource().getExtent());
+});
+
+const style = new Style({
+  fill: new Fill({
+    color: 'black',
+  }),
+});
+background.on('postrender', function (e) {
+
+  const vectorContext = getVectorContext(e);
+  let ctx = e.context;  
+  let canvas = e.context.canvas;
+  let context = canvas.getContext('2d');
+  
+   
+  e.context.globalCompositeOperation = 'destination-out';
+  clipLayer.getSource().forEachFeature(function (feature) {
+    vectorContext.drawFeature(feature, style);
+  });
+  e.context.globalCompositeOperation = 'source-over';
+  /*
+  var imageData = context.getImageData(0,0,canvas.width,  canvas.height);
+  var data = imageData.data;
+  for (var i = 0; i < data.length; i += 4) {
+    data[i + 3] = data[i];
+  }
+  ctx.putImageData(imageData, 0, 0); 
+  */
+});
+
+base.on('postrender', function (e) {
+  const vectorContext = getVectorContext(e);
+  let ctx = e.context;  
+  let canvas = e.context.canvas;
+  
+  
+  e.context.globalCompositeOperation = 'destination-in';
+  clipLayer.getSource().forEachFeature(function (feature) {
+    vectorContext.drawFeature(feature, style);
+  });
+  e.context.globalCompositeOperation = 'source-over';
+});
+
+
+
+
+const getIcon = function(type) {
+  switch (type) {
+    case 'source':
+      return '\u{f773}'; //https://fontawesome.com/icons/water?style=solid
+      break;
+    case 'moulin':
+      return '\u{f085}'; //https://fontawesome.com/icons/water?style=solid
+      break;
+    case 'etymologie':
+      return '\u{f129}'; //https://fontawesome.com/icons/water?style=solid
+      break;
+    case 'site remarquable':
+      return '\u{f02e}'; //https://fontawesome.com/icons/bookmark?style=regular
+      break;
+    case 'immeuble remarquable':
+      return '\u{f66f}'; //https://fontawesome.com/icons/landmark?style=solid
+      break;
+    case 'auberge':
+      return '\u{f0fc}'; //https://fontawesome.com/icons/beer?style=solid
+      break;
+    case 'manufacture':
+      return '\u{f275}'; //https://fontawesome.com/icons/industry?style=solid
+      break;
+    case 'chateau':
+      return '\u{f447}'; //https://fontawesome.com/icons/chess-rook?style=solid
+      break;
+    case 'centre villageois':
+      return '\u{f67f}'; //https://fontawesome.com/icons/fort-awesome?style=brands
+      break;
+    default:
+      return '\u{f129}'; //https://fontawesome.com/icons/info-circle?style=solid
+      break;
+  }
+}
+
+const getColor = function(type) {
+  switch (type) {
+    case 'source':
+      return 'DarkCyan'; //https://fontawesome.com/icons/water?style=solid
+      break;
+    case 'moulin':
+      return 'Gold'; //https://fontawesome.com/icons/water?style=solid
+      break;
+    case 'etymologie':
+      return 'coral'; //https://fontawesome.com/icons/water?style=solid
+      break;
+    case 'site remarquable':
+      return 'DarkSeaGreen'; //https://fontawesome.com/icons/bookmark?style=regular
+      break;
+    case 'immeuble remarquable':
+      return 'orange'; //https://fontawesome.com/icons/landmark?style=solid
+      break;
+    case 'auberge':
+      return 'brown'; //https://fontawesome.com/icons/beer?style=solid
+      break;
+    case 'manufacture':
+      return 'black'; //https://fontawesome.com/icons/industry?style=solid
+      break;
+    case 'chateau':
+      return 'grey'; //https://fontawesome.com/icons/chess-rook?style=solid
+      break;
+    case 'centre villageois':
+      return 'cyan'; //https://fontawesome.com/icons/fort-awesome?style=brands
+      break;
+    default:
+      return 'beige'; //https://fontawesome.com/icons/info-circle?style=solid
+      break;
+  }
+}
+
+const createTextStyle = function(type) {
+    const iconStyle = new Style({
+        image: new Circle({
+            fill: new Fill({color:  getColor(type)}),
+            stroke: new Stroke({color: 'black', width: '2'}),
+            points: 4,
+            radius: 12
+        }),
+        text: new Text({        
+            textAlign: 'center', //center, end, left, right, start
+            textBaseline: 'middle',//alphabetic, bottom, hanging, ideographic, middle, top
+            font: 'bold 12px / 1.6 "Font Awesome 5 Free"', //Arial, Courier New ,... //weight + ' ' + size + '/' + height + ' ' + dom.font.value
+            text: getIcon(type),
+            placement: 'point',
+            offsetX: 0,
+            offsetY: 0,
+            fill: new Fill({color: 'white'}),
+            stroke: new Stroke({color: 'black',width:2}),
+            rotation:0,
+            overflow:false,
+        })
+    });
+    
+    return iconStyle;
+}
+
+
+
+const dataLayer = new VectorLayer({
+  style: function(feature){ return createTextStyle(feature.get('type')) },
+  source: new VectorSource({
+    features: (new GeoJSON()).readFeatures(history,{ featureProjection: 'EPSG:3857'})
+  }),
+});
+
+
+/****/
+background.getSource().getLegends = function(){};
+base.getSource().getLegends = function(){};
+
+clipLayer.getSource().getLegends = function(){
+  
+  return function(){
+    return [
+      {
+        label:'bassin versant',
+        geometry: 'Polygon',
+        style:new Style({
+          fill: new Fill({color: 'white'}),
+          stroke: new Stroke({color: 'red', width: '2'}),
+        })        
+      },
+      {
+        label:'rivière',
+        geometry: 'LineString',
+        style:new Style({
+          stroke: new Stroke({color: 'red', width: '2'}),
+        })
+        
+      }
+    ];
+  };
+};
+
+
+dataLayer.getSource().getLegends = function(){
+  return function(){  
+    
+    const typologie = ['source','moulin', 'etymologie', 'auberge','manufacture','chateau', 'site remarquable','centre villageois', 'immeuble remarquable']; 
+    const legends = [];
+    typologie.forEach(type => legends.push({label:type,style:createTextStyle(type)}));
+    return legends;
+  };
+  
+};
+
+const olLegend = new controlLegend({collapsed:false})
+
+const map = new Map({
+  controls:[olLegend],
+  //interactions:[],
+  layers: [background, base, clipLayer, dataLayer],
+  target: 'map1',
+  view: new View({
+    rotation:1.2,
+    center: [0, 0],
+    zoom: 0
+  }),
+});
+var extent = clipLayer.getSource().getExtent();
+map.getView().fit(extent, map.getSize());
+
+
+
